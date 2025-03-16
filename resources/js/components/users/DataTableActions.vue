df0e59a93f0d28c0d43972d2ab52fe2416669f3d
Lukáš Paukert <paukert.lukas@gmail.com>
2025-03-16 20:56:59 +0100

    feat: confirm deleting user

--- resources/js/components/users/DataTableActions.vue
+++ resources/js/components/users/DataTableActions.vue
@@ -4,6 +4,16 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { MoreHorizontal } from 'lucide-vue-next'
 import { Link } from '@inertiajs/vue3';
 import { User } from '@/types';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog';
 
 defineProps<{
     user: User
@@ -22,7 +32,32 @@ defineProps<{
         <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuItem><Link href="#">Edit</Link></DropdownMenuItem>
-            <DropdownMenuItem><Link href="#">Delete</Link></DropdownMenuItem>
+
+            <Dialog>
+                <DialogTrigger as-child>
+                    <DropdownMenuItem @select="(e) => e.preventDefault()">Delete</DropdownMenuItem>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader class="space-y-3">
+                        <DialogTitle>Do you really want to delete user {{ $props.user.name }}?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete user {{ $props.user.name }}
+                            and all of his data.
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <DialogFooter class="gap-2">
+                        <DialogClose as-child>
+                            <Button variant="secondary">Cancel</Button>
+                        </DialogClose>
+
+                        <Button variant="destructive">
+                            <button type="submit">Delete</button>
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+
         </DropdownMenuContent>
     </DropdownMenu>
 </template>
