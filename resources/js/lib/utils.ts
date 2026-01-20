8ac27e5c6542286baaefe59d6273904d4736c37c
Lukáš Paukert <paukert.lukas@gmail.com>
2026-01-20 09:04:37 +0100

    feat: add humanize-duration package

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,5 +1,6 @@
 import type { Updater } from '@tanstack/vue-table';
 import { type ClassValue, clsx } from 'clsx';
+import humanizeDuration from 'humanize-duration';
 import { twMerge } from 'tailwind-merge';
 import type { Ref } from 'vue';
 
@@ -9,4 +10,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
     ref.value = typeof updaterOrValue === 'function' ? updaterOrValue(ref.value) : updaterOrValue;
+}
+
+export function formatDuration(seconds: number, largest: number = 1) {
+    return humanizeDuration(seconds * 1000, { largest: largest, round: true });
 }
