ee5d267f4c6bdce48422bc605656aac0c874e42c
Lukáš Paukert <paukert.lukas@gmail.com>
2026-02-05 14:41:09 +0100

    feat: Dockerfile for production environment, force HTTPS for production env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->trustProxies(at: '*');
 
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
